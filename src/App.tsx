b81ebabf5da801264a4db3ae7c6d88b1c876f78f
horaoen <horaoen@gmail.com>
2023-02-28 11:05:13 +0800

    Add PrivateRoute component for authorization checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,10 @@
 import "antd/dist/reset.css";
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage, HousePage, LoginPage, UserPage } from "./pages";
-import { RecoilEnv, RecoilRoot, useRecoilState } from "recoil";
-import { getCurrentUserAtom, getTokenAtom } from "./recoil/atom";
-import axios from "./request/axios";
-import { AxiosError } from "axios";
-import { message, Spin } from "antd";
+import { RecoilEnv, RecoilRoot } from "recoil";
 import { ReservationPage } from "./pages/reservation/ReservationPage";
-import { MainLayout } from "./layouts";
-
-interface PropsType {
-  children?: React.ReactNode;
-}
-
-const PrivateRoute: React.FC<PropsType> = ({ children }) => {
-  // 通过的条件是能获得到curreneUser
-  const [token, setToken] = useRecoilState(getTokenAtom());
-  const navigate = useNavigate();
-  const [currentUser, setCurrentUser] = useRecoilState(getCurrentUserAtom());
-  const [state, setState] = useState<Boolean>(false);
-
-  async function fetchCurrentUser(token: string): Promise<any> {
-    axios.defaults.headers.common["Authorization"] = token;
-    const response = await axios.get("/auth/currentUser");
-    return response.data;
-  }
-
-  function checkCurrentUser(currentUser: any): boolean {
-    return Object.keys(currentUser).length !== 0;
-  }
-
-  async function init() {
-    if (checkCurrentUser(currentUser)) {
-      setState(true);
-    } else if (token) {
-      try {
-        const currentUser = await fetchCurrentUser(token);
-        setCurrentUser(currentUser);
-        setState(true);
-      } catch (e) {
-        if (e instanceof AxiosError) {
-          message.error(e.response?.data.message);
-        }
-        navigate("/login");
-      }
-    } else {
-      const jwt = localStorage.getItem("token");
-      if (jwt) {
-        setToken(jwt);
-        try {
-          const currentUser = await fetchCurrentUser(jwt);
-          setCurrentUser(currentUser);
-          setState(true);
-        } catch (e) {
-          if (e instanceof AxiosError) {
-            message.error(e.response?.data.message);
-          }
-          navigate("/login");
-        }
-      }
-      navigate("/login");
-    }
-  }
-
-  useEffect(() => {
-    init();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return <MainLayout>{children}</MainLayout>;
-};
+import { PrivateRoute } from "./components/PrivateRoute";
 
 const App: React.FC = () => {
   RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,76 @@
+import { message, Spin } from "antd";
+import { AxiosError } from "axios";
+import axios from "../request/axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { MainLayout } from "../layouts";
+import { getTokenAtom, getCurrentUserAtom } from "../recoil/atom";
+
+interface PropsType {
+  children?: React.ReactNode;
+}
+
+export const PrivateRoute: React.FC<PropsType> = ({ children }) => {
+  const [token, setToken] = useRecoilState(getTokenAtom());
+  const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useRecoilState(getCurrentUserAtom());
+
+  async function fetchCurrentUser(token: string): Promise<any> {
+    axios.defaults.headers.common["Authorization"] = token;
+    const response = await axios.get("/auth/currentUser");
+    return response.data;
+  }
+
+  function checkCurrentUser(currentUser: any): boolean {
+    // TODO 检查角色
+    return Object.keys(currentUser).length !== 0;
+  }
+
+  async function setup() {
+    if (!checkCurrentUser(currentUser)) {
+      if (token) {
+        // recoil token
+        try {
+          const res = await fetchCurrentUser(token);
+          if (checkCurrentUser(res)) {
+            setCurrentUser(res);
+          } else {
+            navigate("/login");
+          }
+        } catch (e) {
+          if (e instanceof AxiosError) message.error(e.message);
+          navigate("/login");
+        }
+      } else {
+        // localStorage
+        const jwt = localStorage.getItem("token");
+        if (jwt) {
+          setToken(jwt);
+          try {
+            const res = await fetchCurrentUser(jwt);
+            if (checkCurrentUser(res)) setCurrentUser(res);
+            else navigate("/login");
+          } catch (e) {
+            if (e instanceof AxiosError)
+              message.error(e.response?.data.message);
+            navigate("/login");
+          }
+        } else {
+          navigate("/login");
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    setup();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (checkCurrentUser(currentUser)) {
+    return <MainLayout>{children}</MainLayout>;
+  } else {
+    return <Spin />;
+  }
+};
