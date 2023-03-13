0ff53c213873d71ae552a32ca3d67ecf89c924e5
horaoen <horaoen@gmail.com>
2023-03-13 16:42:33 +0800

    update ssl

--- src/pages/house/components/houseEdit/index.tsx
+++ src/pages/house/components/houseEdit/index.tsx
@@ -80,7 +80,7 @@ export const HouseEdit: React.FC = () => {
 
   const basicProps: UploadProps = {
     name: "uploadFile",
-    action: "https://123.60.59.138:5000/file/upload",
+    action: "http://123.60.59.138:5000/file/upload",
     headers: {
       Authorization: token,
     },
