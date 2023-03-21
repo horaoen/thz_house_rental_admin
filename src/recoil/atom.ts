dd59d24cc28fae3820df7f8491eb3807b5d9d555
horaoen <horaoen@gmail.com>
2023-03-21 20:06:00 +0800

    移除不需要的atom

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,32 +1,9 @@
 import { atom } from "recoil";
-import { User } from "../pages/user/components/userTable";
-import { House } from "../request/type";
 
 export const getTokenAtom = () => {
   return atom({
     key: "token",
     default: "",
-  });
-};
-
-export const getCurrentUserAtom = () => {
-  return atom({
-    key: "currentUser",
-    default: {},
-  });
-};
-
-export const getHouseListAtom = () => {
-  return atom<any>({
-    key: "houseDataSource",
-    default: [],
-  });
-};
-
-export const getUserListAtom = () => {
-  return atom<User[]>({
-    key: "userDataSource",
-    default: [],
   });
 };
 
